c3a8a7af0d29b408156f6c237e2988d1d41ae5c6
kubabuchalski <jakub@buchalski.pl>
2015-12-11 22:23:56 +0100

    Coś poprzednio popsułem - zaczynam od nowa

---
+++ src/test/java/edu/iis/mto/bsearch/SearchResulTest.java
@@ -0,0 +1,14 @@
+package edu.iis.mto.bsearch;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SearchResulTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
