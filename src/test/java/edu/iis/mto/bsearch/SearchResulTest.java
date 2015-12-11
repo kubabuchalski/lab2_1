17fc153d84b5583fa659c8011770b18c49fec871
kubabuchalski <jakub@buchalski.pl>
2015-12-11 23:13:00 +0100

    Chyba zrozumiałem polecenie... Dodałem 2 pierwsze testy z tabeli...

--- src/test/java/edu/iis/mto/bsearch/SearchResulTest.java
+++ src/test/java/edu/iis/mto/bsearch/SearchResulTest.java
@@ -4,11 +4,29 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class SearchResulTest {
-
+public class SearchResulTest 
+{
+	
+	SearchResult sr = new SearchResult();
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void  isFoundTestSeq1OK()
+	{
+		int[] seq = {1};
+		int key = 1;
+		sr = BinarySearch.search(key, seq);
+		System.out.println("1 znajduje siê w sekwencji");
+		assertEquals(true,  sr.isFound());
 	}
+	
+	@Test
+	public void  isFoundTestSeq1Feil()
+	{
+		int[] seq = {1};
+		int key = 2;
+		sr = BinarySearch.search(key, seq);
+		System.out.println("2 nie znajduje siê w sekwencji");
+		assertEquals(false,  sr.isFound());
+	}
+
 
 }
